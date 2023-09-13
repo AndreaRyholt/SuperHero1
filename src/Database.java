12e3f726d9e3bfbcdd810aa29849d41863cb276d
juch0001@stud.kea.dk <juliebreum@Julies-MacBook-Air.local>
2023-09-13 15:36:33 +0200

    tilføjet liste over superhelte i main

--- src/Database.java
+++ src/Database.java
@@ -1,25 +1,42 @@
+import javax.annotation.processing.SupportedOptions;
 import java.util.ArrayList;
 
 public class Database {
 
-    private Superhero[] superheroListe;
-    int count = 0;
+    //private Superhero[] superheroListe;
+    private int count = 0;
 
-    public Database() {
-    this.superheroListe = new Superhero[10];
-    }
+    /*public Database() {
+    this.superheroListe = new Superhero[10];*/
+    private ArrayList<Superhero>superheroListe;
+     public Database() {
+         this.superheroListe = new ArrayList<>();
 
 
-    ArrayList<Superhero> superhero = new ArrayList<>();
-    Superhero superman = new Superhero("Superman", "Clark"," Kent", "Flight", "Human");
-    Superhero batman = new Superhero("Batman", "Bruce", " Wayne", "Strong", "Human" );
-    Superhero spiderman = new Superhero("Spider Man", "Tom", " Holland", "Web", "Human" );
+         //ArrayList<Superhero> superhero = new ArrayList<>();
+         superheroListe.add(new Superhero("Superman", "Clark", " Kent", "Flight", "Human"));
+         superheroListe.add(new Superhero("Batman", "Bruce", " Wayne", "Strong", "Human"));
+         superheroListe.add(new Superhero("Spider Man", "Tom", " Holland", "Web", "Human"));
 
+     }
     //ArrayList<Superhero> superhero = new ArrayList<>();
 
 
-    public void addSuperhero(String superheroName, String firstName, String lastName, String abilities, String creature) {
-        superheroListe[count++] = new Superhero(superheroName, firstName, lastName, abilities, creature);
-    }
+   /* public void addSuperhero(String superheroName, String firstName, String lastName, String abilities, String creature) {
+        if (count < superheroListe.length) {
+            superheroListe[count++] = new Superhero(superheroName, firstName, lastName, abilities, creature);
+        } else {
+            System.out.println("full");
+        }*/
 
+    public void addSuperhero(String superheroName, String firstName, String lastName, String abilities, String creature) {
+            Superhero superhero = new Superhero(superheroName, firstName, lastName, abilities, creature);
+            superheroListe.add(superhero);
+        }
+
+
+    public ArrayList<Superhero> getSuperheroListe() {
+        return superheroListe;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList superheroNames = new ArrayList();
+        Database superoDatabase = new Database();
+        /*ArrayList superheroNames = new ArrayList();
         superheroNames.add("Superman");
         superheroNames.add("Batman");
         superheroNames.add("Spiderman");
 
+
         System.out.println(superheroNames.get(0));
         System.out.println(superheroNames.get(1));
-        System.out.println(superheroNames.get(2));
+        System.out.println(superheroNames.get(2));*/
 
         System.out.println("Welcome to the SUPERHERO UNIVERSE!");
 
-        Scanner welcomeMenu = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         boolean runProgram = true;
 
@@ -24,11 +26,13 @@ public class Main {
             System.out.println("2 See complete list of superhero");
             System.out.println("9 Close");
 
-            int menuNumber = welcomeMenu.nextInt();
+            //int menuNumber = welcomeMenu.nextInt();
+            int menuNumber = scanner.nextInt();
 
             if (menuNumber == 1) {
-                Database superhero = new Database();
                 Scanner superheroInput = new Scanner(System.in);
+
+
                 //superheroInput.useDelimiter("[\\s,]+");
 
                 //Hvordan sætter man input ind i array?
@@ -44,7 +48,19 @@ public class Main {
                 String type = superheroInput.next();
                 System.out.println("You have entered: " + superheroName + " " + firstName + lastName + " " + abilities + " " + type);
 
+                superoDatabase.addSuperhero(superheroName, firstName, lastName, abilities, type);
 
+            } else if (menuNumber == 2) {
+                ArrayList<Superhero> superheroListe = superoDatabase.getSuperheroListe();
+                if (superheroListe.isEmpty()) {
+                    System.out.println("Ingen superhelte.");
+                } else {
+                    System.out.println("Liste over superhelte:");
+                    for (Superhero superhero1 : superheroListe) {
+                        if (superhero1 != null)
+                            System.out.println(" Superhero: "  + superhero1.getSuperheroName() + "\n" + " Name: " + superhero1.getFirstName() + " " + superhero1.getLastName() + "\n" + " Abilities: " + superhero1.getAbilities() + "\n" + " Type: " + superhero1.getType());
+                    }
+                }
             } else if (menuNumber == 9) {
                 System.out.println("We hope to see you again soon");
                 runProgram = false;
@@ -58,6 +74,9 @@ public class Main {
         }
     }
 }
+
+
+//String name  = scanner.nextLine()
 // while (!scanner.hasNextInt()){
 // sout "du skal indaaste et tal"}
 //String name  = scanner.nextLine()
